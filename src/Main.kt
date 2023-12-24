2b756ad0b50ea1156543785d732625edfc440e91
Xgui4 Studio <xgui2004@gmail.com>
2023-12-24 11:47:06 -0500

    refonte complet dee l'applis

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,29 @@
 fun main() {
-    print("Hello, World")
+    var name = userInput("Qui est tu?")
+    greeting(name)
+    displayAsciiArt()
+    userInput("Pressez sur n'importe qu'elle touche pour fermer le programme")
+}
+
+fun userInput(msg: String): String {
+    println(msg)
+    var name = readln() ?: ""
+    return name
+}
+
+fun greeting(name: String) {
+    print("Salue ")
+    print(name)
+    print("!")
+}
+
+fun displayAsciiArt() {
+    println("""
+           (__)                      
+           (oo)                                                     \                 (\_/) (\___/) 
+    /-------\/      __        O             \O/          (\___/)     \ /\     ()_()   (^_^) (='.'=)    _[ ]_       
+   / |     ||      /o)\      /|\             |           (='.'=)     ( )     (='.'=)  (>${'$'}<) ( U U )   \('o')/      
+  *  ||----||      \(o/      / \            / \          (")_(")   .( o ).   (")_(")  (/ \) (")_(")    ( : )       
+     ~~    ~~                                    
+    """)
 }
